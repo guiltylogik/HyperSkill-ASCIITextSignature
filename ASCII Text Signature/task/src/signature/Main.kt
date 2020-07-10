054e2c801c9926573ff4b61d0c129a9e8c8a99f4
Jaspar Ifechi <guiltylogik@gmail.com>
2020-07-10 01:21:26 +0000

    Completed Stage 1/4: Primitive Art

--- ASCII Text Signature/task/src/signature/Main.kt
+++ ASCII Text Signature/task/src/signature/Main.kt
@@ -1,5 +1,9 @@
 package signature
 
 fun main() {
-    println("Hello, World!")
+    val name = "Hyper Skill"
+
+    println(" " + "_".repeat(name.length + 2))
+    println("| $name |")
+    println(" " + "¯".repeat(name.length + 2))
 }
